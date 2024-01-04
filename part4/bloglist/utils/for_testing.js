2f7f2b68e4983d32fe761ed563b993d72b96efb7
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-01-04 19:42:49 +0700

    bring jest into app for testing

---
+++ part4/bloglist/utils/for_testing.js
@@ -0,0 +1,21 @@
+const reverse = (string) => {
+  return string
+  .split('')
+  .reverse()
+  .join('')
+}
+
+const average = (array) => {
+  const reducer = (acc, curr) => {
+    return acc + curr
+  }
+
+  return array.length === 0 
+    ? 0 
+    :array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
+  reverse, 
+  average,
+}
