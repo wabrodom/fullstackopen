d8bac277aaca9c3812098f20073d5ca5653f033d
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-01-12 19:40:56 +0700

    use after all in .test to close mongoose connection instead of globalTeardown

---
+++ part4/bloglist/tests/user_api.test.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+describe('when there is one user in db', () => {
+
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('somePassword', 10)
+    const user = new User({
+      username: 'root',
+      name: 'root',
+      passwordHash
+    })
+
+    await user.save()
+
+  })
+
+  test('creation succeed with new Unique username' , async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'bombom',
+      name: 'bombom',
+      password: 'salainen',
+    }
+
+    await api.post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length +1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
