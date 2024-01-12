d09e33fca6eb5aaf9bce9a9150b16e34bab96300
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-01-12 08:10:47 +0700

    implement create new user. make User model and userRoute to handle POST

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogRouter = require('./controllers/blog')
+const userRouter = require('./controllers/user')
 const middleware = require('./utils/middleware')
 
 
@@ -14,6 +15,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
