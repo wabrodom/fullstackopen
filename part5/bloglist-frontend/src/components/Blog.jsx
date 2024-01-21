b470fd3c5a9bfb17ee177dca9b9f087a57595032
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-01-22 06:58:40 +0700

    done 5.7 and add style to Blog component and implemented ahead and done ex 5.8

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,44 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) =>  {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '': 'none'}
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+   <div style={blogStyle}>
+       <div>
+          {blog.title}
+          <button onClick={() => setVisible(!visible)}>
+            {visible ? 'hide': 'view'}
+          </button>
+      </div>
+
+      <section style={showWhenVisible}>
+        <div>
+          <a href={ `${blog.url}` }>{blog.url}</a>
+        </div>
+        <div>
+          likes {blog.likes}
+        </div>
+        <div>
+          {blog.author} 
+        </div>
+    
+
+      </section>
+  
+  
+   </div>  
+  )
+}
+
 
 export default Blog
