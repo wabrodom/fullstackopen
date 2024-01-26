6191b5d76e94507d9040b26294816b2572f325d9
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-01-26 08:21:34 +0700

    do ex.5.14 use @testing-library/user-event to simulate user in ui test if click the button view hide or show the extra content

--- part5/bloglist-frontend/src/components/Blog.unit.test.js
+++ part5/bloglist-frontend/src/components/Blog.unit.test.js
@@ -1,44 +1,98 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test(`a blog renders the blog's title and author by default, the others section is hide`, () => {
-  const blog = {
-    title: "Think again",
-    author: "adamgrant",
-    url: "https://adamgrant.net/",
-    likes: 7,
-    user: {
-      username: "Bom",
-      name: "bombom",
-      id: "65a090a1d58e164b0b72dba0"
-    },
-    id: "65a1f836276b19bec039a885"
-  }
+const blog = {
+  title: "Think again",
+  author: "adamgrant",
+  url: "https://adamgrant.net/",
+  likes: 7,
+  user: {
+    username: "Bom",
+    name: "bombom",
+    id: "65a090a1d58e164b0b72dba0"
+  },
+  id: "65a1f836276b19bec039a885"
+}
 
-  const user ={ id: "65a090a1d58e164b0b72dba0" }
+const user ={ id: "65a090a1d58e164b0b72dba0" }
 
-  const likeABlog = jest.fn()
-  const removeAblog = jest.fn()
 
-  const { container } = render(
-    <Blog 
-      key={blog.id}
-      blog={blog}
-      handleLike={likeABlog}
-      handleDelete={removeAblog}
-      currentUser={user.id}
-    />
-  )
+describe('a blog info renders the Blog component'  , () => {
+  let container
 
-  const titleDiv = container.querySelector('.showByDefaultContent > div:nth-child(1)')
-  const authorDiv = container.querySelector('.showByDefaultContent > div:nth-child(2)')
+  beforeEach(() => {
+    const likeABlog = jest.fn()
+    const removeAblog = jest.fn()
+    
+    container = render(
+        <Blog 
+          key={blog.id}
+          blog={blog}
+          handleLike={likeABlog}
+          handleDelete={removeAblog}
+          currentUser={user.id}
+        />
+      ).container
+  })
 
-  expect(titleDiv).toHaveTextContent('Think again')
-  expect(authorDiv).toHaveTextContent('adamgrant')
+  test(`the blog's title and author by default`, () => {
+    const titleDiv = container.querySelector('.showByDefaultContent > div:nth-child(1)')
+    const authorDiv = container.querySelector('.showByDefaultContent > div:nth-child(2)')
+    
+    expect(titleDiv).toHaveTextContent('Think again')
+    expect(authorDiv).toHaveTextContent('adamgrant')
+    
+    
+  })
+  
+  test('the another section is hide', () => {
+    const sectionBelow = container.querySelector('section:nth-Child(2)')
+    expect(sectionBelow).toHaveStyle('display: none')
+  })
 
-  const sectionBelow = container.querySelector('section:nth-Child(2)')
-  expect(sectionBelow).toHaveStyle('display: none')
+})
+
+describe('when click view or hide button', () => {
+  let container
+
+  beforeEach(() => {
+    const likeABlog = jest.fn()
+    const removeAblog = jest.fn()
+    
+    container = render(
+        <Blog 
+          key={blog.id}
+          blog={blog}
+          handleLike={likeABlog}
+          handleDelete={removeAblog}
+          currentUser={user.id}
+        />
+      ).container
+  })
+
+
+  test('clicking the button will show url and likes', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const section = container.querySelector('section:nth-child(2)')
+    expect(section).not.toHaveStyle('display: none')
+  })
+
+  test('click the button again will hide url and likes', async () => {
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    
+    const hideButton = screen.getByText('hide')
+    await user.click(hideButton)
+
+    const section = container.querySelector('section:nth-child(2)')
+    expect(section).toHaveStyle('display: none')
+  })
 
 })
