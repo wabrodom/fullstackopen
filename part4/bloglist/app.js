14374cd1274ef2179fb5f68246b6feefa221df6f
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-01-30 11:53:46 +0700

    set up cypress to test e2e

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -20,6 +20,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/login', loginRouter)
 app.use('/api/users', userRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
