4334b62bbccb7c3c4d885b14f828ffa180159397
wabrodom <113647599+wabrodom@users.noreply.github.com>
2024-03-05 16:09:54 +0700

    use react-select lib. in place of select tag for some exp with this lib

--- part8/library-frontend/src/components/EditAuthorBirth.jsx
+++ part8/library-frontend/src/components/EditAuthorBirth.jsx
@@ -1,9 +1,12 @@
-import { gql, useMutation } from "@apollo/client"
+import { useMutation } from "@apollo/client"
 import { useState } from "react"
+import Select from 'react-select'
+
 import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries"
 
 const EditAuthorBirth = ({ authors, setError }) => {
   const [year, setYear] = useState('')
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const [ editAuthor ] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{query: ALL_AUTHORS}],
@@ -15,10 +18,35 @@ const EditAuthorBirth = ({ authors, setError }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const currentSelect = event.target.selectAuthor.value
+    // const currentSelect = event.target.selectAuthor.value
 
-    editAuthor({variables: { name: currentSelect, setBornTo: +year} })
+    editAuthor({variables: { name: selectedOption.value, setBornTo: +year} })
   } 
+
+  const options = authors.map(obj => {
+    return { value: obj.name, label: obj.name }
+  })
+  const inputStyle ={ marginTop: '1rem'}
+
+  return (
+    <div>
+      <h3>Edit author birth year</h3>
+      <form onSubmit={handleSubmit}>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={options}
+        />
+
+        <input 
+          value={year}
+          onChange={({target}) => setYear(target.value)}
+          style={inputStyle}
+        />    
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
 
   return (
     <div>
